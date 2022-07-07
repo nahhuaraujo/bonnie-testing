43d3a27ebc517e0c10f3b905f72cb7aab2a3f7ca
nahhuaraujo <nahhuaraujo@gmail.com>
2022-07-07 16:13:01 -0300

    Add option to disable button

--- src/components/ChangingColorButton/ChangingColorButton.test.js
+++ src/components/ChangingColorButton/ChangingColorButton.test.js
@@ -2,50 +2,39 @@ import { render, screen, fireEvent } from '@testing-library/react';
 
 import ChangingColorButton from './ChangingColorButton';
 
-test('Components initial conditions are ok', () => {
-  // No olvidemos renderizar el componente!
+test('Button and checkbox initial text, color, and display', () => {
   render(<ChangingColorButton />);
 
-  // Verificamos que el boton este 'enabled' al iniciar
   const button = screen.getByRole('button', { name: /change to blue/i });
-  expect(button).toBeEnabled();
+  const checkbox = screen.getByRole('checkbox', { name: /disable button/i });
 
-  // Verificamos que el checkbox no este checkeado al iniciar
-  const checkbox = screen.getByRole('checkbox');
+  expect(button).toBeEnabled();
+  expect(button).toHaveStyle({ backgroundColor: 'red' });
+
   expect(checkbox).not.toBeChecked();
 });
 
-test('Button has correct initial color and changes with a click', () => {
-  // Renderizamos el componente
+test('Button changes color and text with a click', () => {
   render(<ChangingColorButton />);
 
-  // Seleccionamos al boton con una query
-  // De este modo verificamos el renderizado, y tambien que tenga el texto correcto
   const button = screen.getByRole('button', { name: /change to blue/i });
 
-  // Verificamos que el background sea 'red'
-  expect(button).toHaveStyle({ backgroundColor: 'red' });
-
-  // Disparamos el evento de click en el boton
   fireEvent.click(button);
 
-  // Verificamos que luego del click haya cambiado a 'blue'
   expect(button).toHaveStyle({ backgroundColor: 'blue' });
-
-  // Con el cambio de color, verificamos el texto del boton haya cambiado a 'red'
   expect(button.textContent).toBe('Change to red');
+
+  fireEvent.click(button);
+
+  expect(button).toHaveStyle({ backgroundColor: 'red' });
+  expect(button.textContent).toBe('Change to blue');
 });
 
-test('Button starts enabled and toggles whenever checkbox is clicked', () => {
+test('Button is disabled while checkbox is marked', () => {
   render(<ChangingColorButton />);
 
   const checkbox = screen.getByRole('checkbox');
   const button = screen.getByRole('button');
-
-  // screen.getByRole('checkbox', { name: /disable button/i }); // Se puede especificar el elemento con name si hay mas de uno usando un label
-
-  expect(checkbox).not.toBeChecked();
-  expect(button).toBeEnabled();
 
   fireEvent.click(checkbox);
 
@@ -56,4 +45,16 @@ test('Button starts enabled and toggles whenever checkbox is clicked', () => {
 
   expect(checkbox).not.toBeChecked();
   expect(button).toBeEnabled();
+});
+
+test('Button is grey while checkbox is marked', () => {
+  render(<ChangingColorButton />);
+
+  const checkbox = screen.getByRole('checkbox');
+  const button = screen.getByRole('button');
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox).toBeChecked();
+  expect(button).toHaveStyle({ backgroundColor: 'grey' });
 });
