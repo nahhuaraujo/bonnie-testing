6574ed2bc4a1575db0b5d86bb48e54eafc4a3d86
nahhuaraujo <nahhuaraujo@gmail.com>
2022-07-14 15:42:01 -0300

    Implement color constants for components and tests

--- src/components/ChangingColorButton/ChangingColorButton.js
+++ src/components/ChangingColorButton/ChangingColorButton.js
@@ -1,39 +1,36 @@
 import { useState } from 'react';
 
+import { replaceCamelCaseWithSpaces } from '../../util/util';
+import { COLORS } from '../../constants/constants';
+
 const ChangingColorButton = () => {
-  const [buttonColor, setButtonColor] = useState('red');
+  const [buttonColor, setButtonColor] = useState(COLORS.MEDIUM_VIOLET_RED);
   const [isDisabled, setIsDisabled] = useState(false);
 
-  const newColor = buttonColor === 'red' ? 'blue' : 'red';
+  let newColor = buttonColor === COLORS.MEDIUM_VIOLET_RED ? COLORS.MIDNIGHT_BLUE : COLORS.MEDIUM_VIOLET_RED;
+  let style = { backgroundColor: buttonColor };
 
-  let style;
+  if (isDisabled) style = { backgroundColor: COLORS.GREY, color: COLORS.WHITE };
+  else if (buttonColor === COLORS.MEDIUM_VIOLET_RED) style = { ...style };
+  else if (buttonColor === COLORS.MIDNIGHT_BLUE) style = { ...style, color: COLORS.WHITE };
 
-  if (isDisabled) style = { backgroundColor: 'grey', color: 'white' };
-  else if (buttonColor === 'red') style = { backgroundColor: buttonColor };
-  else if (buttonColor === 'blue')
-    style = { backgroundColor: buttonColor, color: 'white' };
+  const buttonClickHandler = () => {
+    setButtonColor(newColor);
+  };
+
+  const radioClickHandler = e => {
+    setIsDisabled(e.target.checked);
+  };
 
   return (
     <div>
       <div>
-        <button
-          style={style}
-          disabled={isDisabled}
-          onClick={() => {
-            setButtonColor(newColor);
-          }}
-        >
-          Change to {newColor}
+        <button style={style} disabled={isDisabled} onClick={buttonClickHandler}>
+          Change to {replaceCamelCaseWithSpaces(newColor)}
         </button>
       </div>
       <div>
-        <input
-          id='disable-button-checkbox'
-          type='checkbox'
-          onClick={e => {
-            setIsDisabled(e.target.checked);
-          }}
-        />
+        <input id='disable-button-checkbox' type='checkbox' onClick={radioClickHandler} />
         <label htmlFor='disable-button-checkbox'>Disable button</label>
       </div>
     </div>
--- src/components/ChangingColorButton/ChangingColorButton.test.js
+++ src/components/ChangingColorButton/ChangingColorButton.test.js
@@ -2,14 +2,17 @@ import { render, screen, fireEvent } from '@testing-library/react';
 
 import ChangingColorButton from './ChangingColorButton';
 
+import { replaceCamelCaseWithSpaces } from '../../util/util';
+import { COLORS } from '../../constants/constants';
+
 test('Button and checkbox initial text, color, and display', () => {
   render(<ChangingColorButton />);
 
-  const button = screen.getByRole('button', { name: /change to blue/i });
-  const checkbox = screen.getByRole('checkbox', { name: /disable button/i });
+  const button = screen.getByRole('button', { name: `Change to ${replaceCamelCaseWithSpaces(COLORS.MIDNIGHT_BLUE)}` });
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
 
   expect(button).toBeEnabled();
-  expect(button).toHaveStyle({ backgroundColor: 'red' });
+  expect(button).toHaveStyle({ backgroundColor: COLORS.MEDIUM_VIOLET_RED });
 
   expect(checkbox).not.toBeChecked();
 });
@@ -17,17 +20,17 @@ test('Button and checkbox initial text, color, and display', () => {
 test('Button changes color and text with a click', () => {
   render(<ChangingColorButton />);
 
-  const button = screen.getByRole('button', { name: /change to blue/i });
+  const button = screen.getByRole('button', { name: `Change to ${replaceCamelCaseWithSpaces(COLORS.MIDNIGHT_BLUE)}` });
 
   fireEvent.click(button);
 
-  expect(button).toHaveStyle({ backgroundColor: 'blue' });
-  expect(button.textContent).toBe('Change to red');
+  expect(button).toHaveStyle({ backgroundColor: COLORS.MIDNIGHT_BLUE });
+  expect(button.textContent).toBe(`Change to ${replaceCamelCaseWithSpaces(COLORS.MEDIUM_VIOLET_RED)}`);
 
   fireEvent.click(button);
 
-  expect(button).toHaveStyle({ backgroundColor: 'red' });
-  expect(button.textContent).toBe('Change to blue');
+  expect(button).toHaveStyle({ backgroundColor: COLORS.MEDIUM_VIOLET_RED });
+  expect(button.textContent).toBe(`Change to ${replaceCamelCaseWithSpaces(COLORS.MIDNIGHT_BLUE)}`);
 });
 
 test('Button is disabled while checkbox is marked', () => {
@@ -56,5 +59,5 @@ test('Button is grey while checkbox is marked', () => {
   fireEvent.click(checkbox);
 
   expect(checkbox).toBeChecked();
-  expect(button).toHaveStyle({ backgroundColor: 'grey' });
+  expect(button).toHaveStyle({ backgroundColor: COLORS.GREY });
 });
